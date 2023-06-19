2703f49f3578981a27ecd5237eaf14838100f460
zhangdahai112 <631871336@qq.com>
2023-06-20 00:23:04 +0800

    chore: 调试docker

--- webkvm/startup.sh
+++ webkvm/startup.sh
@@ -10,24 +10,24 @@ if [ -z $VENDOR ];then
   exit 1
 fi
 
-if [[ $VENDOR == DEL* ]];then
+if [[ "$VENDOR" == DEL* ]];then
   dir=/dell
-elif [[ $VENDOR == Inspur* ]];then
+elif [[ "$VENDOR" == Inspur* ]];then
   dir=/inspur
-elif [[ $VENDOR == *H3C* ]];then
+elif [[ "$VENDOR" == *H3C* ]];then
   dir=/h3c
-elif [[ $VENDOR == Supermicro* ]];then
+elif [[ "$VENDOR" == Supermicro* ]];then
   dir=/supermicro
-elif [[ $VENDOR == Suma* ]];then
+elif [[ "$VENDOR" == Suma* ]];then
   dir=/sugo
-elif [[ $VENDOR == Lenovo* ]];then
+elif [[ "$VENDOR" == Lenovo* ]];then
   dir=/lenovo
 else
   echo "Not supported Vendor: $VENDOR"
   exit 1
 fi
 
-/bin/bash $dir/startup.sh
+/bin/bash $pwd/$dir/startup.sh
 
 if [ ! -f /app/jviewer.jnlp ];then
   echo "Failed to get jviewer.jnlp please add QQ:631871336 for support"
